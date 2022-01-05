cddb88ba4bdbf2c0fe4075d239649feb6335ab44
Alex Chan <chan.alexkf@hutchisonports.com>
2022-01-05 23:03:24 +0800

    Alex

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	private static final Logger LOGGER = LogManager.getLogger(HelloController.class);
+
 	@GetMapping("/")
 	public String index() {
+		LOGGER.info("[HelloController](index)");
 		return "Greetings from Spring Boot!";
 	}
 
